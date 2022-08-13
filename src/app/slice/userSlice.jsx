5cf9e7398c8fd988bf6be0739dc34a686a221587
devyouth94 <devyouth94@gmail.com>
2022-08-14 00:55:10 +0900

    jwt토큰 관련 함수 완료

--- src/app/slice/userSlice.jsx
+++ src/app/slice/userSlice.jsx
@@ -1,18 +1,30 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { server_url } from ".";
+import jwt_decode from "jwt-decode";
 
 const initialState = {
-  users: [],
-  isLoading: false,
+  users: {},
   error: null,
+};
+
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
 };
 
 const __postLogin = createAsyncThunk("/login", async (value, thunkAPI) => {
   try {
     // const { data } = await axios.post(`${server_url}/login`, value);
     const { data } = await axios.get(`${server_url}/login`);
-    return thunkAPI.fulfillWithValue(data.token);
+    const token = data.token;
+    localStorage.setItem("jwtToken", token);
+    setAuthToken(token);
+    // console.log(jwt_decode(token));
+    return thunkAPI.fulfillWithValue(jwt_decode(token));
   } catch (error) {
     return thunkAPI.rejectWithValue(error);
   }
@@ -22,7 +34,15 @@ const userSlice = createSlice({
   name: "userSlice",
   initialState,
   reducers: {},
+  extraReducers: {
+    [__postLogin.fulfilled]: (state, action) => {
+      state.users = action.payload;
+    },
+    [__postLogin.rejected]: (state, action) => {
+      state.error = action.payload;
+    },
+  },
 });
 
-export { __postLogin };
+export { setAuthToken, __postLogin };
 export default userSlice;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { setAuthToken, __postLogin } from "./app/slice/userSlice";
+import jwt_decode from "jwt-decode";
+
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  store.dispatch(__postLogin(jwt_decode(localStorage.jwtToken)));
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
